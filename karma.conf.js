db15b77cd8f11bfc8cb1e3bee11c78c3f58d5e65
Joko Mulyono <joko.mulyono@akarinti.tech>
2022-08-18 13:21:44 +0700

    init project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/heros'),
+      dir: require('path').join(__dirname, './coverage/angular-skeleton'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'heros'`, () => {
+  it(`should have as title 'angular-skeleton'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('heros');
+    expect(app.title).toEqual('angular-skeleton');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('heros app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-skeleton app is running!');
   });
 });
