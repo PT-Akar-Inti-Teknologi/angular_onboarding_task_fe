fe6a4c0dc1672908ac879e3bdb9f312f047b34e2
latif akar inti <moch.saepul@akarinti.tech>
2022-12-05 21:03:14 +0700

    feat(login): add userInformation and isAuthenticated

--- src/app/core/guards/auth.service.ts
+++ src/app/core/guards/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { UserInformation } from 'src/app/shared/interfaces/user-model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,17 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  get userInformation(): UserInformation{
+    const getUser = JSON.parse(sessionStorage.getItem("userInformation")!);
+    return getUser as UserInformation;
+  }
+
+  isAuthenticated(): Observable<boolean> {
+    if(this.userInformation !== null || this.userInformation !== undefined){
+      return of(true);
+    }
+
+    return of(false);
+  }
 }
