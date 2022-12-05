d509c430b3a2bf6c7ee679a0adcf0fe6d72829e9
latif akar inti <moch.saepul@akarinti.tech>
2022-12-05 20:33:38 +0700

    feat(login): add login by token and email

--- src/app/modules/login/services/login.service.ts
+++ src/app/modules/login/services/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, find } from 'rxjs';
 import { UserInformation, UserModel } from 'src/app/shared/interfaces/user-model';
 
 @Injectable({
@@ -14,15 +14,23 @@ export class LoginService {
     private http: HttpClient
   ) { }
 
-  onSignIn(params: {email: string, password: string }): Observable<UserModel>{
+  onSignIn(params: {email: string, password: string, token?: string }): Observable<UserModel>{
     return this.http.get<UserModel[]>(this.linkUserModel).pipe(
       map(res => {
         
-        const findUser = res.find(el => el.email === params.email && el.password === params.password);
+        const findUser = res.find(el => { 
+          
+          if(params.token !== undefined || params.token !== null){
+            return el.email === params.email && el.token === params.token 
+          }
+
+          return el.email === params.email && el.password === params.password
+        });
 
         if(findUser !== undefined) {
           
           const params: UserInformation = {
+            email: findUser.email,
             name: findUser.name,
             avatar: findUser.avatar,
             token: findUser.token
