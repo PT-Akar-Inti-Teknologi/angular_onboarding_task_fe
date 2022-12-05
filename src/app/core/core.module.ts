19779d491acf73ceda281d5b0a6b10235126820c
latif akar inti <moch.saepul@akarinti.tech>
2022-12-05 14:46:57 +0700

    feat: login
     - import martial ui & config app routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 
+const materialUiModules = [
+  MatCardModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    materialUiModules
+  ],
+  exports: [
+    materialUiModules
   ]
 })
 export class CoreModule { }
