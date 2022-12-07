4dd72d7080d2280133fe7c98744fe520a2a8017d
latif akar inti <moch.saepul@akarinti.tech>
2022-12-07 11:16:45 +0700

    feat(dashboard): create interface products

---
+++ src/app/modules/dashboard/modules/products/interface/products-model.ts
@@ -0,0 +1,13 @@
+export interface ProductsModel {
+    createdAt: string;
+    name: string;
+    image: string;
+    color: string;
+    product: string;
+    description: string;
+    material: string;
+    adjective: string;
+    price: string;
+    slug: string;
+    id: string;
+}
