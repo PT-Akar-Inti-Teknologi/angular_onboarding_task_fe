612f501d2b78cd820fd26ff5ac85251fa1a6cec5
latif akar inti <moch.saepul@akarinti.tech>
2022-12-07 12:33:10 +0700

    feat(auth): add router sign out and page not found, add Auth Guard and No Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { NoAuthGuard } from './core/guards/no-auth.guard';
+import { LogoutPageComponent } from './modules/auth/components/logout-page/logout-page.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
-  { path: 'sign-in', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
-  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: 'sign-in', canActivate: [NoAuthGuard], loadChildren: () => import('./modules/auth/login.module').then(m => m.LoginModule) },
+  { path: 'dashboard', canActivate: [AuthGuard], loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'sign-out', component: LogoutPageComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
